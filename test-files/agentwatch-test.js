b77167714bf8fbb97a0ec84e2e7a7d2d8f83db2d
Jim Wiese <jim@fortifiedstrength.org>
2025-08-06 21:56:12 -0700

    Test: Add test file for AgentWatch V2 verification

---
+++ test-files/agentwatch-test.js
@@ -0,0 +1,12 @@
+// Test file for AgentWatch system
+// This file tests the fixed workflows with proper GITHUB_TOKEN
+
+function testAgentWatch() {
+  console.log('Testing AgentWatch V2 workflows');
+  console.log('- GITHUB_TOKEN properly configured');
+  console.log('- Shell-quote parsing implemented');
+  console.log('- Modular workflow design');
+  return true;
+}
+
+module.exports = { testAgentWatch };
